69a9fdc78168a69610f30a429c5b6a327f8d786d
Ryan Oldja <ryanoldja@gmail.com>
2017-12-14 16:16:05 -0500

    move print statement inside init

--- roboschool/gym_mujoco_walkers.py
+++ roboschool/gym_mujoco_walkers.py
@@ -53,9 +53,9 @@ class RoboschoolAnt(RoboschoolForwardWalkerMujocoXML):
         return +1 if z > 0.26 else -1  # 0.25 is central sphere rad, die if it scrapes the ground
 
 class RoboschoolAntESE680(RoboschoolForwardWalkerMujocoXML):
-    print('YAY Creating new custom Ant agent.')
     foot_list = ['front_left_foot', 'front_right_foot', 'left_back_foot', 'right_back_foot']
     def __init__(self):
+        print('YAY Creating new custom Ant agent.')
         RoboschoolForwardWalkerMujocoXML.__init__(self, "ant.xml", "torso", action_dim=8, obs_dim=28, power=2.5)
     def alive_bonus(self, z, pitch):
         return +1 if z > 0.26 else -1  # 0.25 is central sphere rad, die if it scrapes the ground
